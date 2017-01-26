9930859d3f060a3d311397f753304f2132656e1a
Juan David Sánchez <juancho85@gmail.com>
2017-01-26 21:12:19 +0100

    add models for location and place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AddPlacePage} from "../pages/add-place/add-place";
+import {SetLocationPage} from "../pages/set-location/set-location";
+import {PlacePage} from "../pages/place/place";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    SetLocationPage,
+    PlacePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +20,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    SetLocationPage,
+    PlacePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/models/place.ts
@@ -0,0 +1,7 @@
+export class Place {
+
+  constructor(public title: string,
+              public description: string,
+              public location: Location,
+              public imagePath: string) {}
+}
